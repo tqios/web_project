d5eddff04af58c0d7dc2f829f1085d522a081116
OH-JOU-YEON <91864044+OH-JOU-YEON@users.noreply.github.com>
2021-11-21 23:26:30 +0900

    login.html changes login button type input

---
+++ login.js
@@ -0,0 +1,17 @@
+const myInfo ={
+    id:"hyperclutter",
+    password:"11111"
+};
+
+
+const login=()=>{
+    const id =document.querySelector('#id');
+    const pw =document.querySelector('#pw');
+
+    if(id.value ===myInfo.id||pw.value===myInfo.password){
+        location.href='myinfo.html'
+    }
+    else{
+        alert("로그인 실패")
+    }
+}
